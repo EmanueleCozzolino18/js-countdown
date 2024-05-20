1b344846fd739d307edb2446d84d9a2ddc1e92bf
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-20 14:22:10 +0200

    [UPD] Creata la funzione che esegue il conto alla rovescia in console.log

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@
 "use strict";
 
 console.log("Lo script funziona correttamente")
+
+let timerSecond = 10;
+
+//scriviamo una funzione che esegue il conto alla rovescia 
+function contoRovescio() {
+
+    if (timerSecond > 0) {
+
+        console.log(timerSecond);
+        timerSecond--;
+    }
+
+}
+
+//Eseguiamo la funzione contoRovescio ogni secondo
+let timer = setInterval(contoRovescio, 1000);
