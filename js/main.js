a2cc02ef2c2a034786a15fdaa7aaeb06dd1c870b
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-20 14:24:24 +0200

    [UPD] QUANDO ARRIVA A 0 CI STAMPERA BUON ANNO IN CONSOLE

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@ let timerSecond = 10;
 //scriviamo una funzione che esegue il conto alla rovescia 
 function contoRovescio() {
 
+    //CONDIZIONE SE TIMER SECOND E' MAGGIORE DI 0 
     if (timerSecond > 0) {
 
         console.log(timerSecond);
         timerSecond--;
-    }
+        //QUANDO SARA' ARRIVATO A 0 CI STAMPERA BUON ANNO IN CONSOLE
+    } else console.log("Buon Anno!");
 
 }
 
